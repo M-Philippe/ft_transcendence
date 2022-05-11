c519da5a4848c713b28c961e1594c30c659a8da0
nnieddu <nicolas.nieddu@free.fr>
2022-05-11 17:05:37 +0200

    up design, add gitignore

--- frontend/src/header/loginInfos/loginInfos.tsx
+++ frontend/src/header/loginInfos/loginInfos.tsx
@@ -7,7 +7,7 @@ function LoginInfos(props: any) {
       return (
         <div>
           {/* <p className="login">Hello {props.user.username}</p> */}
-          <img className = "avatar" style={{width: "7vw", height: "9vh"}} src={props.user.avatar} alt="avatarUser" />
+          <img className = "avatar" style={{width: "6vw", height: "9vh"}} src={props.user.avatar} alt="avatarUser" />
       </div>
       );
     }
