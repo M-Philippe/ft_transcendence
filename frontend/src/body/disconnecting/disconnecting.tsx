ef773d826e048941f5dfa90a87c707bb501a3860
M-Philippe <m.philippe59@gmail.com>
2022-05-22 11:20:45 +0200

    resolved leaks in disconnecting.tsx

--- frontend/src/body/disconnecting/disconnecting.tsx
+++ frontend/src/body/disconnecting/disconnecting.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import store from "../../store/store";
+import { useEffect, useState } from "react";
 import { DISCONNECT_USER } from "../../store/userSlice/userSliceActionTypes";
 import { API_USER_DISCONNECT, BASE_URL } from "../../urlConstString";
 import { Navigate } from "react-router-dom";
+import { connect } from "react-redux";
+import { DispatchType, storeState } from "../../store/types";
+import { userState } from "../../store/userSlice/userSliceTypes";
 
-export default function Disconnecting(props: any) {
+function Disconnecting(props: { user: userState, dispatch: DispatchType }) {
 	const [leaving, setLeaving] = useState(false);
 	
+	useEffect(() => {
+		if (props.user.isConnected)
+			props.dispatch({
+				type: DISCONNECT_USER,
+				user: props.user,
+			});
+	})
+
 	fetch(API_USER_DISCONNECT, {
 		method: "put",
 		headers: {"Content-Type": "application/json"},
@@ -17,16 +27,17 @@ export default function Disconnecting(props: any) {
 		setLeaving(true);
 	});
 
-	if (store.getState().user.isConnected) {
-		store.dispatch({
-			type: DISCONNECT_USER,
-			user: {},
-		});
-	} 
-	if (leaving) {
-			return(<Navigate to={BASE_URL} />
+	if (!props.user.isConnected) {
+		return(<Navigate to={BASE_URL} />
 		);
 	}
-		return (<Navigate to={BASE_URL} />
-	);
+	return (<p>Leaving.</p>)
 }
+
+function mapStateToProps(state: storeState) {
+	return ({
+		user: state.user,
+	});
+}
+
+export default connect(mapStateToProps)(Disconnecting);
