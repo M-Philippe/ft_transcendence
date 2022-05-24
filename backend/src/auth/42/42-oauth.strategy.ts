a39b5a9245d8d0d963925c36d3010c57cad95b45
M-Philippe <m.philippe59@gmail.com>
2022-05-24 10:14:51 +0200

    resolved unsefined linked bug in chat

--- backend/src/auth/42/42-oauth.strategy.ts
+++ backend/src/auth/42/42-oauth.strategy.ts
@@ -13,11 +13,12 @@ export class OAuth42Strategy extends PassportStrategy(Strategy, '42') {
         private readonly usersService: UsersService
     ) {
         super({
-            authorizationURL: 'https://api.intra.42.fr/oauth/authorize?client_id=ed0b86fd834fae7e8171d1f8dc0a84ef3586cec062339193fe00184dbf7fa685&redirect_uri=http%3A%2F%2F10.1.4.3%3A3000%2Fauth%2F42%2Fredirect&response_type=code',
+            //authorizationURL: 'https://api.intra.42.fr/oauth/authorize?client_id=ed0b86fd834fae7e8171d1f8dc0a84ef3586cec062339193fe00184dbf7fa685&redirect_uri=http%3A%2F%2F10.1.4.3%3A3000%2Fauth%2F42%2Fredirect&response_type=code',
+            authorizationURL: 'https://api.intra.42.fr/oauth/authorize?client_id=ed0b86fd834fae7e8171d1f8dc0a84ef3586cec062339193fe00184dbf7fa685&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2F42%2Fredirect&response_type=code',
             tokenURL: 'https://api.intra.42.fr/oauth/token',
             clientID: configService.get<string>("OAUTH_42_UID"),
             clientSecret: configService.get<string>("OAUTH_42_SECRET"),
-            callbackURL: "http://10.1.4.3:3000/auth/42/redirect",
+            callbackURL: "http://localhost:3000/auth/42/redirect",
             scope: 'public',
             proxy: true,
 
