dea6114e95e1dc42ce7931813a9d9693f0d2613c
nnieddu <nicolas.nieddu@free.fr>
2022-05-26 16:43:51 +0200

    clean chatLine (console.log) and decrease sleep to 200

--- frontend/src/body/chat/chatConnected/chatLine.tsx
+++ frontend/src/body/chat/chatConnected/chatLine.tsx
@@ -45,7 +45,7 @@ export default function ChatLine(props: IChatLineProps) {
     setAnchorElNav(null);
     props.socket.emit("postMessage", {id: props.id, username: null, message: "/mp " + props.username});
     
-    await sleep(200);
+    await sleep(400);
     let i = props.state.lstId.length - 2;
       props.dispatch({
         type: "UPDATE_FOCUS_AND_REMOVE_GREEN",
