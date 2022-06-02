449371c92667b38343320c82ac35d9ca9c5e2c11
M-Philippe <m.philippe59@gmail.com>
2022-06-02 15:25:08 +0200

    footer disappear when ingame

--- frontend/src/footer/footer.tsx
+++ frontend/src/footer/footer.tsx
@@ -4,12 +4,8 @@ import { storeState } from "../store/types";
 // TODO Need to avoid actualisation needed to align footer
 function Footer(props: { isInGame: boolean, isConnected: boolean }) 
 {
-  if (!props.isInGame && !props.isConnected)
-    return (
-      <footer style={{color:'white'}}>
-      © pminne vroth-di ninieddu
-      </footer>
-    );
+  if (props.isInGame)
+    return (null);
   else
     return (
       <footer style={{paddingRight:"24vw", color:'white'}}>
