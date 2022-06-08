61441bdef63cc07ddba3909f0428bff6b5c2a922
nnieddu <nicolas.nieddu@free.fr>
2022-06-08 18:39:21 +0200

    clean

--- frontend/src/header/loginInfos/loginInfos.tsx
+++ frontend/src/header/loginInfos/loginInfos.tsx
@@ -3,13 +3,10 @@ import {connect} from "react-redux";
 import Avatar from '@mui/material/Avatar';
 
 function LoginInfos(props: any) {
-//console.log("AVATAR: ", props.user.avatar);
   if (props.user.isConnected) {
       return (
-        <div>
-          {/* <p className="login">Hello {props.user.username}</p> */}
+      <div>
 				<Avatar alt="avatar" src={props.user.avatar} />
-          {/* <img id = "avatarHeader" className="imgHeader" src={props.user.avatar} alt="avatarUser" /> */}
       </div>
       );
     }
